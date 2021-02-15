36141f6b46d48154d5f107bddf678365b3c1105a
Daniels <nezinu69@gmail.com>
2021-02-15 14:43:01 +0200

    Papildināts

--- Pizza-Petrisins.java
+++ Pizza-Petrisins.java
@@ -1,6 +1,24 @@
 import java.text.DecimalFormat;
 public class Pizza-Petrisins {
+	public static void main(String[] args) {
 	DecimalFormat df = new DecimalFormat("#.##");
-	Scanner keyboard = new Scanner(System.in);
+	Scanner scan = new Scanner(System.in);
+	
+	//Mainîgie
+	DecimalFormat df = new DecimalFormat("#.##");
+	Scanner scan = new Scanner(System.in);
+	String vards;
+	char garozas tips;
+	int cm;
+	double apmaksa = 0.0;
+	double summa;
+	double cena;
+	System.out.println("Ievadiet savu Vârdu un Uzvârdu: );"
+	Vards = keyboard.nextLine();
+	
+	//Picas cenas
+	System.out.println("Kâda izmçra picu jûs vçlçtos?: );"
+	
+	
 	
 }
